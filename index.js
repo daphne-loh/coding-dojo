9c2e22052cb22002e0b75bef024d5d4ae71054ea
Daphne Loh <daphne@redairship.com>
2019-09-09 17:03:21 +0800

    Add solution for your order please kata.

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const yourOrderPlease = () => {
-	throw 'No solution provided!';
+const yourOrderPlease = (words) => {
+  var word = words.split(" ");
+  var newArr = [];
+
+  for (var i=0; i < word.length; i++) {
+    for (var j=0; j < word.length; j++) {
+      var numb = (word[j].match(/\d/g))-1
+
+      if (i === numb){
+        newArr[i] = word[j];
+      }
+    }
+
+
+  }
+
+  return(newArr.join(" "));
 }
 export default yourOrderPlease;
