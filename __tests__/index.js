13a45a0217afd9ad66162bea5637bfb632c858c0
Daphne Loh <daphne@redairship.com>
2019-09-09 14:15:15 +0800

    Fix test cases error.

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,9 +3,9 @@ import persistantBugger from '../index';
 describe('Code kata', () => {
 	it('should return the number', () => {
 		expect(persistantBugger(39)).toEqual(3);
-		expect(persistantBugger(4)).toEqual(3);
-		expect(persistantBugger(25)).toEqual(3);
-		expect(persistantBugger(999)).toEqual(3);
+		expect(persistantBugger(4)).toEqual(0);
+		expect(persistantBugger(25)).toEqual(2);
+		expect(persistantBugger(999)).toEqual(4);
   });
 });
 
