403bcf2ac3d445beaeac4bb3061d1ab10e36b00c
Daphne Loh <daphne@redairship.com>
2019-09-09 14:16:29 +0800

    Add solution for persistant bugger

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-const persistantBugger = () => {
-	throw 'No solution provided!';
+const persistantBugger = (num) => {
+  var count = 0;
+  var str = num.toString();
+
+  if (str.length === 1)
+    count = 0;
+
+  else {
+
+    do {
+
+      var mult = 1;
+
+      for (var i=0; i < str.length; i++){
+        mult *= str[i];
+      }
+
+      num = mult;
+      str = num.toString();
+      count++;
+
+    }while (str.length > 1);
+
+  }
+
+  return count;
 }
 export default persistantBugger;
